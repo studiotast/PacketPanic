ac4f1a89e47b95cd76f18fad50d04e33991de087
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2025-03-26 14:33:01 +0100

    add clear and clean up calls to useBalls

--- src/stores/useBalls.js
+++ src/stores/useBalls.js
@@ -1,13 +1,30 @@
-import create from "zustand";
+import { create } from "zustand";
 
-const useBalls = create((set) => ({
+export default create((set) => ({
   balls: [],
   addBall: (ball) => set((state) => ({ balls: [...state.balls, ball] })),
   // Verwijder een bal op basis van de id
-  removeBall: (id) =>
+  removeBall: (id) => {
     set((state) => ({
       balls: state.balls.filter((ball) => ball.id !== id),
-    })),
-}));
+    }));
+  },
 
-export default useBalls;
+  clearBalls: () => {
+    set({ balls: [] });
+  },
+
+  // Optional: Add a method to clean up balls that have fallen below a certain Y position
+  cleanupBalls: () => {
+    set((state) => ({
+      balls: state.balls.filter((ball) => {
+        // If we have a ref and can check position, filter out balls below -10
+        if (ball.ref.current) {
+          const position = ball.ref.current.position;
+          return position.y > -10;
+        }
+        return true; // Keep balls without refs
+      }),
+    }));
+  },
+}));
