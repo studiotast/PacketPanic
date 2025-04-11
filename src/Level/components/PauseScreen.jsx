74282099d9ed24321948a3aa70cbb0e26fea1424
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2025-04-11 08:46:20 +0200

    WIP: pause screen

--- src/Level/components/PauseScreen.jsx
+++ src/Level/components/PauseScreen.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import useGame from "../../stores/useGame";
+import Layout from "../../Layout";
+import Button from "../../Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/pro-solid-svg-icons";
 
 export default function PauseScreen() {
   const togglePause = useGame((state) => state.togglePause);
 
   return (
-    <div className="pause-screen">
+    <Layout>
       <div className="pause-content">
-        <h2>Game Paused</h2>
+        <h2>Gepauzeerd</h2>
         <p>Press P to resume</p>
-        <button className="resume-button" onClick={togglePause}>
-          Resume Game
-        </button>
+        <Button onClick={togglePause}>
+          Verder spelen
+          <FontAwesomeIcon icon={faPlay} style={{ marginLeft: "10px" }} />
+        </Button>
       </div>
-    </div>
+    </Layout>
   );
 }
