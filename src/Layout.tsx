3f446dd04b0b25a363088676e254049c05b052a7
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2025-04-11 11:06:59 +0200

    add mute icon/button to layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,15 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import useGame from "./stores/useGame";
+import { faVolume, faVolumeSlash } from "@fortawesome/pro-solid-svg-icons";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
+  const mute = useGame((state) => state.mute);
+  const unMute = useGame((state) => state.unMute);
   return (
     <div className="layout-container">
+      <div className="mute-icon" onClick={mute ? unMute : undefined}>
+        <FontAwesomeIcon icon={mute ? faVolumeSlash : faVolume} />
+      </div>
       <div className="layout-inner">
-        <img alt="bg" src="/images/bg.jpg" className="layout-bg" />
         {children}
+        <img alt="bg" src="/images/bg.jpg" className="layout-bg" />
       </div>
     </div>
   );
