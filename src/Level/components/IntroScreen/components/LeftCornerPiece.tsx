1680beb037e66d9023a34645bbcfad282ece719d
liekevdvoort <49523639+liekevdvoort@users.noreply.github.com>
2025-04-23 13:35:05 +0200

    Add icon

--- src/Level/components/IntroScreen/components/LeftCornerPiece.tsx
+++ src/Level/components/IntroScreen/components/LeftCornerPiece.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { faArrowRight } from "@fortawesome/pro-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function LeftCornerPiece() {
   return (
@@ -7,6 +9,7 @@ export default function LeftCornerPiece() {
         <img src="/assets/images/left-corner-piece.svg" />
         <div className="left-corner-piece-content">
           <p>Alle ervaringen bekijken</p>
+          <FontAwesomeIcon fill="#000" icon={faArrowRight} />
         </div>
       </div>
     </div>
