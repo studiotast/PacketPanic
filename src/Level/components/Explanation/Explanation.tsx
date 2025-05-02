79960c178d972bae125c7a92aecd2eaf1b6dea43
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2025-05-02 10:45:44 +0200

    add start tutorial to explanation

--- src/Level/components/Explanation/Explanation.tsx
+++ src/Level/components/Explanation/Explanation.tsx
@@ -5,6 +5,7 @@ import useGame from "../../../stores/useGame";
 export default function Explanation() {
   const playSound = useGame((state) => state.playSound);
   const currentLevel = useGame((state) => state.currentLevel);
+  const startTutorial = useGame((state) => state.startTutorial);
 
   const [explanationIndex, setExplanationIndex] = useState(
     currentLevel.storyLine.length - currentLevel.storyLine.length
@@ -41,8 +42,13 @@ export default function Explanation() {
           className="explanation-button"
           onClick={() => {
             if (readyToStart) {
-              useGame.setState({ phase: "ready" });
-              return;
+              if (typeof window.initiatePhaseTransition === "function") {
+                window.initiatePhaseTransition("tutorial");
+                return;
+              } else {
+                startTutorial();
+                return;
+              }
             }
             playSound("robotTalking");
             setExplanationIndex((prev) => prev + 1);
