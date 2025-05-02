3cad59ae5335b523415cdb1560f55011268a9bf0
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2025-05-02 10:45:05 +0200

    add tutorial screen component

---
+++ src/Level/components/TutorialScreen.tsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import TvWrapper from "./TvWrapper";
+import Button from "../../Button";
+import useGame from "../../stores/useGame";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/pro-solid-svg-icons";
+
+export default function TutorialScreen() {
+  const playSound = useGame((state) => state.playSound);
+  const isMuted = useGame((state) => state.isMuted);
+  const currentLevel = useGame((state) => state.currentLevel);
+
+  const [readyToStart, setReadyToStart] = useState(false);
+
+  const [tutorialIndex, setTutorialIndex] = useState(0);
+
+  useEffect(() => {
+    if (tutorialIndex === currentLevel.tutorial.length - 1) {
+      setReadyToStart(true);
+    } else {
+      setReadyToStart(false);
+    }
+  }, [tutorialIndex]);
+
+  useEffect(() => {
+    // Play sound when the component mounts
+    const sound = playSound("level");
+
+    // Return cleanup function to stop sound on unmount
+    return () => {
+      if (sound) {
+        sound.pause();
+        sound.currentTime = 0;
+      }
+    };
+  }, [playSound, isMuted]);
+
+  return (
+    <TvWrapper>
+      <div className="tutorial-content">
+        {/* Background image */}
+        <img
+          alt="Tutorial background"
+          src="/assets/images/tutorial.png"
+          className="tutorial-bg"
+        />
+
+        {/* Bottom container for positioning elements */}
+        <div className="tutorial-bottom-container">
+          {/* Text in bottom left */}
+          <div className="tutorial-text">
+            <p>{currentLevel.tutorial[tutorialIndex].text}</p>
+          </div>
+
+          {/* Button in bottom right */}
+          <div className="tutorial-button-wrapper">
+            <Button
+              className="tutorial-button"
+              onClick={() => {
+                if (readyToStart) {
+                  if (typeof window.initiatePhaseTransition === "function") {
+                    window.initiatePhaseTransition("ready");
+                    return;
+                  } else {
+                    useGame.setState({ phase: "ready" });
+                    return;
+                  }
+                }
+                setTutorialIndex((prev) => prev + 1);
+              }}
+              shadowColor="#dc9329"
+            >
+              Verder
+              <FontAwesomeIcon icon={faCircleCheck} />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </TvWrapper>
+  );
+}
