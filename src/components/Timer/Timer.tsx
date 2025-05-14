6dfff37c473fb900f01eb064981a92e910fea7a3
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2025-05-14 17:13:36 +0200

    update timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,23 +1,24 @@
-import useGame from "../../stores/useGame.js";
-import "../../style.css";
+import useGame from "../../stores/useGame";
+import styles from "./Timer.module.scss";
 
 export default function Timer() {
   const timer = useGame((state) => state.timer);
-  const phase = useGame((state) => state.phase);
   const currentLevel = useGame((state) => state.currentLevel);
 
   // Format time: convert seconds to MM:SS format
-  const formatTime = (timeInSeconds) => {
+  const formatTime = (timeInSeconds: number): string => {
     const timeLeft = currentLevel.timeLimit - timeInSeconds;
     const minutes = Math.floor(timeLeft / 60);
     const seconds = Math.floor(timeLeft % 60);
+
     if (seconds < 0) {
       return "00:00";
     }
+
     return `${minutes.toString().padStart(2, "0")}:${seconds
       .toString()
       .padStart(2, "0")}`;
   };
 
-  return <div className="time">{formatTime(timer)}</div>;
+  return <div className={styles.time}>{formatTime(timer)}</div>;
 }
