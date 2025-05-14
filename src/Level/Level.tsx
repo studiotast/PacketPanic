f3fbc8b55d4042cebceedf6364d72ed6e79df47c
Sachin Lendis <67464439+komfysach@users.noreply.github.com>
2025-05-14 16:59:41 +0200

    change level to tsx

--- src/Level/Level.tsx
+++ src/Level/Level.tsx
@@ -1,12 +1,7 @@
-import { useMemo } from "react";
 import * as THREE from "three";
-import BlockAxe from "./components/BlockAxe";
-import BlockLimbo from "./components/BlockLimbo";
-import BlockSpinner from "./components/BlockSpinner";
-import Track from "./components/CustomLevel/Track";
+import useGame from "../stores/useGame";
 import TrackLevel1 from "./components/CustomLevel/Tracks/TrackLevel1/TrackLevel1";
 import TrackLevel2 from "./components/CustomLevel/Tracks/TrackLevel2/TrackLevel2";
-import useGame from "../stores/useGame";
 
 // reusable geometries
 export const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
@@ -99,33 +94,11 @@ export const traingleMaterial = new THREE.MeshStandardMaterial({
   side: THREE.DoubleSide, // Ensure both sides are rendered
 });
 
-export default function Level({
-  count = 5,
-  types = [BlockSpinner, BlockAxe, BlockLimbo],
-  seed = 0,
-}) {
+export default function Level() {
   const currentLevelId = useGame((state) => state.currentLevelId);
-
-  const blocks = useMemo(() => {
-    const blocks = [];
-
-    for (let i = 0; i < count; i++) {
-      const type = types[Math.floor(Math.random() * types.length)];
-      blocks.push(type);
-    }
-
-    return blocks;
-  }, [count, types, seed]);
 
   return (
     <>
-      {/* <BlockStart position={[0, 0, 0]} />
-      {blocks.map((Block, index) => (
-        <Block key={index} position={[0, 0, -(index + 1) * 4]} />
-      ))}
-      <BlockEnd position={[0, 0, -(count + 1) * 4]} /> */}
-      {/* <Bounds length={count + 2} /> */}
-      {/* <Track /> */}
       {currentLevelId == 1 && <TrackLevel1 />}
       {currentLevelId == 2 && <TrackLevel2 />}
     </>
